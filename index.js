b587f20bb740eca7cbe60724b7ab0c1c00301b74
pixelthin <ajqajq@gmail.com>
2020-02-26 06:51:13 -0600

    Fixes a bug that caused the wrong package.json to be read

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ const argv = require('yargs')
     .argv
 // Pull in a custom config file or package.json if it exists.
 const config = argv.config ? require(argv.config) : {}
-const package = existsSync('./package.json') ? require('./package.json') : {}
+const packageFile = `${process.cwd()}/package.json`
+const packageFound = existsSync(packageFile)
+const package = packageFound ? require(packageFile) : {}
 
 // Input is a regular string that may contain JS template string syntax
 // Create a template string from the input string and eval it to
 // get a string to execute via the command-line interpreter
 let templateString = "`" + argv._[0] + "`"
 const toExecute = eval(templateString)
+if (packageFound) {
+    console.log(`conex: package.json found at: ${packageFile}`)
+} else {
+    console.log(`conex: no package.json found at ${packageFile}`)
+}
 console.log(`conex: executing ${toExecute}`)
+
 const child = exec(toExecute)
 child.stdout.on('data', data => console.log(data))
 child.stderr.on('data', data => console.error(data))
